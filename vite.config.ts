1c6b0d5dfb7be10370b3ddde600ff6b8d2ce18e1
Rory Anderson <rory.anderson@globalpay.com>
2026-04-29 16:36:15 +0300

    page base from envvar or /v1/

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from "node:url";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 
-const GITHUB_PAGES_BASE = "/poi-viz/";
+const GITHUB_PAGES_BASE = process.env.VITE_BASE_PATH ?? "/poi-viz/v1/";
 
 export default defineConfig(({ command }) => ({
   base: command === "build" ? GITHUB_PAGES_BASE : "/",
